e13a45856e38f76f80e5ed19e5ef3542cb47f027
uniquedapoet <eddiebenjamin86@gmail.com>
2024-10-30 14:16:46 -0600

    Simulate Day

--- CashAppIntegration/CashAppStockManagment.py
+++ CashAppIntegration/CashAppStockManagment.py
@@ -1,17 +1,17 @@
+import joblib
+import pandas as pd
+from SimulateDay import stock_market_simulation, train_model, get_stock_data
 import os
 import sys
 
 # Add the main directory to the Python path
 sys.path.append('/Users/eduardobenjamin/Desktop/Repos/StockTradingModel')
 
-from SimulateDay import stock_market_simulation, train_model, get_stock_data
-import pandas as pd
-import joblib
-
 
 def simulate_day_for_cash_app():
     portfolio = pd.read_csv("CashAppIntegration/portfolio.csv")
-    daysimulation = pd.DataFrame(columns=['Stock Name','Day','Action','Stock Price','Cash','Shares Held','Portfolio Value','Date'])
+    daysimulation = pd.DataFrame(columns=[
+                                 'Stock Name', 'Day', 'Action', 'Stock Price', 'Cash', 'Shares Held', 'Portfolio Value', 'Date'])
     cash = portfolio['Cash'].iloc[-1]
     for stock in portfolio['Stock Name'].unique():
         try:
@@ -20,25 +20,30 @@ def simulate_day_for_cash_app():
             print(f"Training model for {stock}...")
             model = train_model(stock)
             joblib.dump(model, f"models/LGBMmodels/{stock}_model.pkl")
-        
-        stock_data = get_stock_data(stock).tail(1)
+
+        updated_stock_df = get_stock_data(stock)
+        updated_stock_df = updated_stock_df.tail(1)
+
         day = (portfolio[portfolio['Stock Name'] == stock]['Day'].iloc[-1]) + 1
+
         new_row, _ = stock_market_simulation(
             model,
             initial_cash=cash,
             days=1,
-            stock=stock_data,
-            existing_shares=portfolio[portfolio['Stock Name'] == stock]['Shares Held'].iloc[-1],
+            stock=updated_stock_df,
+            existing_shares=portfolio[portfolio['Stock Name']
+                                      == stock]['Shares Held'].iloc[-1],
             oneDay=day,
             descision=portfolio[portfolio['Stock Name'] == stock]
         )
         cash = new_row['Cash'].iloc[-1]
         daysimulation = pd.concat([daysimulation, new_row])
-    
-    # Save the updated portfolio back to the CSV file  
-    daysimulation['Cash'] = daysimulation['Cash'].iloc[-1]     
+
+    # Save the updated portfolio back to the CSV file
+    daysimulation['Cash'] = daysimulation['Cash'].iloc[-1]
     daysimulation.fillna(0, inplace=True)
-    daysimulation.to_csv("CashAppIntegration/portfolio.csv", index=False, mode='a', header=False)
+    daysimulation.to_csv("CashAppIntegration/portfolio.csv",
+                         index=False, mode='a', header=False)
     return portfolio
 
 
