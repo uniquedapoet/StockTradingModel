665e65b94a349d63ab624dd223c63c2db4cc6cbe
uniquedapoet <eddiebenjamin86@gmail.com>
2024-10-31 09:37:27 -0600

    Simuliate Day and provide temp fix for cashapp function.

--- CashAppIntegration/CashAppStockManagment.py
+++ CashAppIntegration/CashAppStockManagment.py
@@ -1,47 +1,58 @@
+import joblib
+import pandas as pd
+from SimulateDay import stock_market_simulation, train_model, get_stock_data
 import os
 import sys
 
 # Add the main directory to the Python path
 sys.path.append('/Users/eduardobenjamin/Desktop/Repos/StockTradingModel')
 
-from SimulateDay import stock_market_simulation, train_model, get_stock_data
-import pandas as pd
-import joblib
-
 
 def simulate_day_for_cash_app():
+    """
+    !!! Currently has an issue with making decisions when given only one day.
+    Current fix ==  feeding it 5 days and only selecting most recent day to record!!!
+    """
     portfolio = pd.read_csv("CashAppIntegration/portfolio.csv")
-    daysimulation = pd.DataFrame(columns=['Stock Name','Day','Action','Stock Price','Cash','Shares Held','Portfolio Value','Date'])
+    daysimulation = pd.DataFrame(columns=['Stock Name', 'Day', 'Action', 'Stock Price',
+                                 'Cash', 'Shares Held', 'Portfolio Value', 'Date', 'Actual Sell'])
     cash = portfolio['Cash'].iloc[-1]
     for stock in portfolio['Stock Name'].unique():
-        try:
-            model = joblib.load(f"models/LGBMmodels/{stock}_model.pkl")
-        except FileNotFoundError:
-            print(f"Training model for {stock}...")
-            model = train_model(stock)
-            joblib.dump(model, f"models/LGBMmodels/{stock}_model.pkl")
-        
-        updated_stock_df = get_stock_data(stock)
-        updated_stock_df = updated_stock_df.tail(1)
+        updated_stock_df = get_stock_data(stock).drop_duplicates()
+        updated_stock_df = updated_stock_df.reset_index(drop=True)
+        updated_stock_df = updated_stock_df.tail(5)
 
         day = (portfolio[portfolio['Stock Name'] == stock]['Day'].iloc[-1]) + 1
 
+        # Load the specific model for the stock, or fallback to the general model if it doesn't exist
+        try:
+            specific_model = joblib.load(
+                f'models/LGBMmodels/{stock}_model.pkl')
+            print(f"Using model for {stock}")
+        except Exception:
+            print(f"Model for {stock} not found. Training a new model...")
+            specific_model = train_model(stock)
+            print(f"Model trained for {stock}")
+        # Simulate a day of trading for the stock with the specific model
         new_row, _ = stock_market_simulation(
-            model,
+            model=specific_model,
             initial_cash=cash,
-            days=1,
+            days=5,  # Simulate only one day
             stock=updated_stock_df,
-            existing_shares=portfolio[portfolio['Stock Name'] == stock]['Shares Held'].iloc[-1],
             oneDay=day,
+            existing_shares=portfolio[portfolio['Stock Name']
+                                      == stock]['Shares Held'].iloc[-1],
+            masstrades=True,
             descision=portfolio[portfolio['Stock Name'] == stock]
         )
-        cash = new_row['Cash'].iloc[-1]
+        cash += new_row['Cash'].iloc[-1]
         daysimulation = pd.concat([daysimulation, new_row])
-    
-    # Save the updated portfolio back to the CSV file  
-    daysimulation['Cash'] = daysimulation['Cash'].iloc[-1]     
+
+    # Save the updated portfolio back to the CSV file
+    daysimulation['Cash'] = daysimulation['Cash'].iloc[-1]
     daysimulation.fillna(0, inplace=True)
-    daysimulation.to_csv("CashAppIntegration/portfolio.csv", index=False, mode='a', header=False)
+    daysimulation.to_csv("CashAppIntegration/portfolio.csv",
+                         index=False, mode='a', header=False)
     return portfolio
 
 
--- SimulateDay.py
+++ SimulateDay.py
@@ -111,6 +111,7 @@ def stock_market_simulation(model,
                  'Shares Held', 'Portfolio Value', 'Stock Price']) if descision is None else descision)
     days = min(days, len(stock))
     actualSell = None
+
     # Go through each day and make a decision based on the model
     for i in range(days):
         # Get the stock price, strategy and day for the current day
@@ -153,7 +154,7 @@ def stock_market_simulation(model,
                       stock_price}, Cash left: {cash}")
 
         # Sell shares if the strategy is 'Sell' and shares are held
-        elif (strategy == 'Sell') and (shares_held > 0) and (modelDecisionDf['Action'].tail(2).eq('Sell').all()):
+        elif (strategy == 'Sell') and (shares_held > 0) and (modelDecisionDf['Action'].tail(2).eq('Sell').all()) and len(modelDecisionDf) > 1:
             # If the last 5 actions were 'Sell', sell 5 shares and mass trade is enabled
             actualSell = True
             if (modelDecisionDf['Action'].tail(5) == 'Sell').all() and shares_held >= 5 and masstrades:
@@ -760,6 +761,7 @@ def get_stock_data(symbol: str
             })
             row.to_csv('data/sp500_stocks.csv',
                        index=False, mode='a', header=False)
+    stock_df['Date'] = pd.to_datetime(stock_df["Date"])
     return stock_df
 
 
@@ -970,8 +972,6 @@ def simulate_days(days: int,
             updated_stock_df = get_stock_data(symbol)
             updated_stock_df = updated_stock_df.tail(days)
 
-            # updated_stock_df = stock_data[stock_data['Symbol'] == symbol].tail(1)
-
             # Load the specific model for the stock, or fallback to the general model if it doesn't exist
             try:
                 specific_model = joblib.load(
@@ -1155,17 +1155,58 @@ def simulate_day_specific(model_type: str = 'LGBM'
                                mode='a', header=False, index=False)
 
 
+def simulate_day_for_cash_app():
+    portfolio = pd.read_csv("CashAppIntegration/portfolio.csv")
+    daysimulation = pd.DataFrame(columns=['Stock Name','Day','Action','Stock Price','Cash','Shares Held','Portfolio Value','Date'])
+    cash = portfolio['Cash'].iloc[-1]
+    for stock in portfolio['Stock Name'].unique():
+        try:
+            model = joblib.load(f"models/LGBMmodels/{stock}_model.pkl")
+        except FileNotFoundError:
+            print(f"Training model for {stock}...")
+            model = train_model(stock)
+            joblib.dump(model, f"models/LGBMmodels/{stock}_model.pkl")
+        
+        updated_stock_df = get_stock_data(stock)
+        updated_stock_df = updated_stock_df.tail(1)
+
+        day = (portfolio[portfolio['Stock Name'] == stock]['Day'].iloc[-1]) + 1
+
+        new_row, _ = stock_market_simulation(
+            model,
+            initial_cash=cash,
+            days=1,
+            stock=updated_stock_df,
+            existing_shares=portfolio[portfolio['Stock Name'] == stock]['Shares Held'].iloc[-1],
+            oneDay=day,
+            descision=portfolio[portfolio['Stock Name'] == stock]
+        )
+        cash = new_row['Cash'].iloc[-1]
+        daysimulation = pd.concat([daysimulation, new_row])
+    
+    # Save the updated portfolio back to the CSV file  
+    daysimulation['Cash'] = daysimulation['Cash'].iloc[-1]     
+    daysimulation.fillna(0, inplace=True)
+    daysimulation.to_csv("CashAppIntegration/portfolio.csv", index=False, mode='a', header=False)
+    return portfolio
+
+
+
+
 if __name__ == '__main__':
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_dir)
     warnings.filterwarnings('ignore')
     # Simulate one day for all stocks, continuing from previous cash balances
-    simulate_days(1000,
+    portfolio = pd.read_csv('CashAppIntegration/portfolio.csv')
+    simulate_days(5,
                   to_file=True,
                   massTrade=True,
                   cash=10000,
-                  file_location='simResults/1000_day_test.csv',
+                  file_location='simResults/1_day_test.csv',
+                  symbols = portfolio['Stock Name'].unique()
 )
+    # simulate_day_for_cash_app()
     # simulate_day_specific('LGBM')
     # train_models()
     # scale_and_obtain_data('AAPL')
